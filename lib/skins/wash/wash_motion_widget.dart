c6da0ac4349be31f436659603359396d525e2ebb
yonghyeon <yhsofa89@gmail.com>
2026-08-16 18:36:57 +0900

    refactor: wash 모션 초기 상태를 타이머 상태에서 유도, 전이 로직 순수함수로 통합

--- lib/skins/wash/wash_motion_widget.dart
+++ lib/skins/wash/wash_motion_widget.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:gif_view/gif_view.dart';
 import 'package:joguman_pomodoro/providers/data_provider.dart';
+import 'package:joguman_pomodoro/skins/wash/wash_motion_logic.dart';
 import 'package:joguman_pomodoro/widgets/my_gif.dart';
 import 'package:provider/provider.dart';
 
@@ -19,19 +19,28 @@ class _WashMotionWidgetState extends State<WashMotionWidget> {
   final controllerStart = GifController();
   final controllerActivate = GifController();
   final controllerStop = GifController();
-  int gifIndex = 0;
-  bool isLoaded = false;
+  late WashState _state;
+  late final WashState _initialState; // 마운트 시 1회 고정 — autoPlay 대상 결정용
+
+  GifController _controllerOf(WashState state) {
+    switch (state) {
+      case WashState.blink:
+        return controllerBlink;
+      case WashState.start:
+        return controllerStart;
+      case WashState.activate:
+        return controllerActivate;
+      case WashState.stop:
+        return controllerStop;
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    initFunc();
-  }
-
-  initFunc() async {
-    setState(() {
-      isLoaded = true;
-    });
+    _initialState =
+        washInitialState(isStarted: context.read<DataProvider>().isStarted);
+    _state = _initialState;
   }
 
   @override
@@ -43,58 +52,25 @@ class _WashMotionWidgetState extends State<WashMotionWidget> {
     super.dispose();
   }
 
-  void callbackBlink() {
-    bool isStarted = context.read<DataProvider>().isStarted;
-    if (isStarted) {
-      gifIndex = 1;
-      controllerBlink.stop();
-      controllerStart.play();
+  Future<void> _onFinish(WashState finished) async {
+    if (!mounted) return;
+    final bool isStarted = context.read<DataProvider>().isStarted;
+    final WashState next =
+        washNextState(finished: finished, isStarted: isStarted);
+    if (next == finished) {
+      // 같은 모션 반복: blink는 loop=true라 seek(0)만, activate는 재생을 다시 건다
+      final controller = _controllerOf(finished);
+      controller.seek(0);
+      if (finished == WashState.activate) {
+        await Future.delayed(const Duration(milliseconds: 1));
+        controller.play();
+      }
     } else {
-      controllerBlink.seek(0);
+      _controllerOf(finished).stop();
+      _controllerOf(next).play();
     }
-    setState(() {});
-  }
-
-  void callbackStart() {
-    bool isStarted = context.read<DataProvider>().isStarted;
-    if (isStarted) {
-      gifIndex = 2;
-      controllerStart.stop();
-      controllerActivate.play();
-    } else {
-      gifIndex = 3;
-      controllerStart.stop();
-      controllerStop.play();
-    }
-    setState(() {});
-  }
-
-  Future<void> callbackActivate() async {
-    bool isStarted = context.read<DataProvider>().isStarted;
-    if (isStarted) {
-      controllerActivate.seek(0);
-      await Future.delayed(const Duration(milliseconds: 1));
-      controllerActivate.play();
-    } else {
-      gifIndex = 3;
-      controllerActivate.stop();
-      controllerStop.play();
-    }
-    setState(() {});
-  }
-
-  void callbackStop() {
-    bool isStarted = context.read<DataProvider>().isStarted;
-    if (isStarted) {
-      gifIndex = 1;
-      controllerStop.stop();
-      controllerStart.play();
-    } else {
-      gifIndex = 0;
-      controllerStop.stop();
-      controllerBlink.play();
-    }
-    setState(() {});
+    if (!mounted) return;
+    setState(() => _state = next);
   }
 
   @override
@@ -102,17 +78,35 @@ class _WashMotionWidgetState extends State<WashMotionWidget> {
     return Selector<DataProvider, bool>(
       selector: (context, dataProvider) => dataProvider.isStarted,
       builder: (context, isStarted, child) {
-        if (isStarted && gifIndex == 0) {
-          gifIndex = 1;
+        // 화면에 보이는 중 시작 조작을 목격하면 즉시 start 전환 모션 재생
+        // (정지는 기존 동작대로 현재 모션이 끝나는 시점에 _onFinish에서 전이)
+        if (isStarted && _state == WashState.blink) {
+          _state = WashState.start;
+          controllerBlink.stop();
           controllerStart.play();
         }
         return IndexedStack(
-          index: gifIndex,
+          index: _state.index,
           children: [
-            MyGif(image: 'assets/gif/wash/wash_blink.gif', callback: callbackBlink, controller: controllerBlink, autoPlay: true, loop: true),
-            MyGif(image: 'assets/gif/wash/wash_start.gif', callback: callbackStart, controller: controllerStart),
-            MyGif(image: 'assets/gif/wash/wash_activate.gif', callback: callbackActivate, controller: controllerActivate),
-            MyGif(image: 'assets/gif/wash/wash_stop.gif', callback: callbackStop, controller: controllerStop),
+            MyGif(
+                image: 'assets/gif/wash/wash_blink.gif',
+                callback: () => _onFinish(WashState.blink),
+                controller: controllerBlink,
+                autoPlay: _initialState == WashState.blink,
+                loop: true),
+            MyGif(
+                image: 'assets/gif/wash/wash_start.gif',
+                callback: () => _onFinish(WashState.start),
+                controller: controllerStart),
+            MyGif(
+                image: 'assets/gif/wash/wash_activate.gif',
+                callback: () => _onFinish(WashState.activate),
+                controller: controllerActivate,
+                autoPlay: _initialState == WashState.activate),
+            MyGif(
+                image: 'assets/gif/wash/wash_stop.gif',
+                callback: () => _onFinish(WashState.stop),
+                controller: controllerStop),
           ],
         );
       },
